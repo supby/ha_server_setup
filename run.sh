fc0fcd2b3d6612bbbf993fd8b0429eeb4a52100e
Andre Suzanovich <supius@gmail.com>
2023-12-28 19:10:19 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Creating necessary directories
+mkdir -p ./ha_server_data/mosquitto/{config,data,log}
+mkdir -p ./ha_server_data/zigbee2mqtt_data
+mkdir -p ./ha_server_data/zigbee2mqtt_2_data
+mkdir -p ./ha_server_data/webthings
+mkdir -p ./ha_server_data/nodered_data
+mkdir -p ./ha_server_data/influx/{config,data}
+
+# run docker-compose
+sudo docker-compose up -d
