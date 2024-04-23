ebaa3addbe20d44ac74ff92703a0ff2e43e6e853
Andre <supius@gmail.com>
2024-04-23 08:20:57 +0200

    add grafana

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ mkdir -p ./ha_server_data/zigbee2mqtt_data
 mkdir -p ./ha_server_data/zigbee2mqtt_2_data
 mkdir -p ./ha_server_data/webthings
 mkdir -p ./ha_server_data/nodered_data
-mkdir -p ./ha_server_data/influx/{config,data}
+mkdir -p ./ha_server_data/grafana/{config,data}
+mkdir -p ./ha_server_data/grafana/{config,data}
 
 # run docker-compose
 sudo docker-compose up -d
